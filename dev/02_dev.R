9b76200a562340e529500ba6630ef240b1993373
hlageek <radim.hladik@fulbrightmail.org>
2021-05-13 00:51:47 +0900

    force rm requal from renv
    after using renv::hydrate(), requal package was automatically added to lockfile, this commit removed requal and commented out covrpage package in 02_dev.R

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -52,7 +52,7 @@ devtools::build_vignettes()
 usethis::use_coverage()
 
 # Create a summary readme for the testthat subdirectory
-covrpage::covrpage()
+# covrpage::covrpage()
 
 ## CI ----
 ## Use this part of the script if you need to set up a CI
