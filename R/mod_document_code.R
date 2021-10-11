7c0c7243900f85a7d2039ffd1d8fb7cd020923a4
Michael Škvrňák <397925@mail.muni.cz>
2021-10-11 13:36:41 +0200

    Js tagging (#21)
    * Add multiple line text
    * Update selection range
    * highlights first version
    
    Co-authored-by: hlageek <radim.hladik@fulbrightmail.org>

--- R/mod_document_code.R
+++ R/mod_document_code.R
@@ -17,11 +17,10 @@ mod_document_code_ui <- function(id){
       )
     ),
     
-    textOutput(ns("focal_text")),
-
-
-
+    actionButton(ns("add_code"), label = "code"),
     
+    htmlOutput(ns("focal_text")),
+
     textOutput(ns("captured_range"))
   )
 }
@@ -33,12 +32,18 @@ mod_document_code_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     
-    lorem_ipsum_input <- "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    lorem_ipsum_input <- paste(c("Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+                               "Sněmovní komise k ekologické katastrofě na řece Bečvě zkritizovala postup při odběru i analýze vzorků vody a ryb bezprostředně po loňské havárii. Podle poslanců nedostatečně koordinovaly kroky vodoprávní úřad Valašského Meziříčí a Česká inspekce životního prostředí. Uvedli to v závěrečné zprávě, kterou má agentura ČTK k dispozici. Otrava přesně před rokem poničila biotop na 40 kilometrech řeky."), 
+                               collapse = "<br/>")
     
-    output$focal_text <- renderText({lorem_ipsum_input})
+    output$focal_text <- renderUI(HTML({lorem_ipsum_input}))
+    
+    observeEvent(input$add_code, {
+
+      golem::invoke_js("highlight", list("yellow"))
 
 
-
+    })
 
     # observeEvent(input$tag_position, {
     # print(input$tag_position)
