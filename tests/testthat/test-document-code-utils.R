d62536be2ccf8af424e507a2d9409c820b41e3a2
Michael Škvrňák <397925@mail.muni.cz>
2021-11-28 15:56:25 +0100

    Add tests of code overlap auxiliary function

--- tests/testthat/test-document-code-utils.R
+++ tests/testthat/test-document-code-utils.R
@@ -1,9 +1,17 @@
 coded_segments_df <- dplyr::tibble(
-    project_id = c(1, 1),
-    code_id = c(1, 1), 
-    doc_id = c(1, 1), 
+    project_id = 1,
+    code_id = 1, 
+    doc_id = 1,
     segment_start = c(10, 30), 
     segment_end = c(20, 40)
+)
+
+coded_segments_overlap <- dplyr::tibble(
+    project_id = 1, 
+    code_id = c(1, 2), 
+    doc_id = 1, 
+    segment_start = c(10, 20), 
+    segment_end = c(30, 40)
 )
 
 test_that("Test overlap checking returns correct nrows", {
@@ -35,4 +43,12 @@ test_that("Test that calculating new segment range works", {
             summarise_new_segment_range(., 15, 35) %>%
             dplyr::pull(segment_end) == 40
     )
+})
+
+test_that("Test that calculating code overlap works", {
+    expect_true(nrow(calculate_code_overlap(coded_segments_df)) == 2)
+    expect_true(nrow(calculate_code_overlap(coded_segments_overlap)) == 3)
+    expect_true(all(calculate_code_overlap(coded_segments_overlap) %>% 
+                        dplyr::pull(code_id) %in% c("1", "1+2", "2")))
+    
 })
