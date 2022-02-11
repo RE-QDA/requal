105ea7f0c849476b513feede4781bce01bf8608b
hlageek <radim.hladik@fulbrightmail.org>
2022-02-11 19:03:56 +0100

    global vars fix

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,9 @@
 # Define project_name as global variable to pass R CMD Check without notes
-utils::globalVariables(c("project_name", "project_id", "doc_id"))
+utils::globalVariables(c("project_name", 
+                         "project_id", 
+                         "doc_id",
+                         "code_name",
+                         "code_id",
+                         "segment_id",
+                         "code_color",
+                         "text"))
