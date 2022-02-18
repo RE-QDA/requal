2aa0ca1c7e8ec454e735a07c722d93396fbcf19c
hlageek <radim.hladik@fulbrightmail.org>
2022-02-18 16:00:42 +0100

    add refresh button, add reactivity to tab selection

--- R/mod_reporting.R
+++ R/mod_reporting.R
@@ -10,12 +10,18 @@
 mod_reporting_ui <- function(id){
   ns <- NS(id)
   tagList(
-    tabsetPanel(type = "tabs", id = "tabset",
+    tabsetPanel(type = "tabs", id = ns("reporting_tabset"),
                 tabPanel("Instructions", 
+                         id = ns("instructions"),
                          value = "instructions",
                          textOutput(ns("report_instructions"))),
                 tabPanel("Logs", 
+                         id = ns("logs"),
                          value = "logs",
+                         actionButton(ns("logs_refresh"),
+                                      label = "",
+                                      icon = icon("refresh")) %>% 
+                           tagAppendAttributes(style = "float:right;"),
                          dataTableOutput(ns("report_logs")))
     )
  
@@ -34,12 +40,12 @@ mod_reporting_server <- function(id, project){
       "Instructions for using this module..."
     )
     
-    logs_df <- eventReactive({
-      project()$active_project
-    }, {
+    logs_df <- eventReactive(input$reporting_tabset == "logs" | input$logs_refresh, {
+     
       if(isTruthy(project()$active_project)){
         load_logs_for_reporting(project()$project_db,
-                                project()$active_project)
+                                project()$active_project) %>% 
+          dplyr::arrange(dplyr::desc(created_at))
       }else{""}
     })
   
