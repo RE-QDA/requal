6a5875ce2ccfced1c23678077679982b35706642
hlageek <radim.hladik@fulbrightmail.org>
2022-02-18 20:51:31 +0100

    update metadata and display version

--- R/app_server.R
+++ R/app_server.R
@@ -36,10 +36,14 @@ app_server <- function(input, output, session) {
  
   segments_observer <- mod_document_code_server("document_code_ui_1", project_observer, codebook, documents)
   
-  # analysis
+  # analysis ----
   
   segments_df <- mod_analysis_server("analysis_ui_1", project_observer, codebook, documents, segments_observer)
 
   mod_download_handler_server("download_handler_ui_1", segments_df)
+  
+  # about -----
+  
+  mod_about_server("about_ui_1", project_observer)
   
 }
