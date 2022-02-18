65c790038b394e89a462662cd9102f99028d432f
フラジク・ラジム <radim.hladik@fulbrightmail.org>
2022-02-18 16:13:01 +0100

    Update log report (#33)
    * Limit payload length
    * Make payload human readable
    
    Co-authored-by: Michael Škvrňák <397925@mail.muni.cz>

--- R/mod_reporting.R
+++ R/mod_reporting.R
@@ -10,10 +10,19 @@
 mod_reporting_ui <- function(id){
   ns <- NS(id)
   tagList(
-    
-    tabsetPanel(type = "tabs", id = "tabset",
-                tabPanel("Instructions", textOutput(ns("report_instructions"))),
-                tabPanel("Logs", dataTableOutput(ns("report_logs")))
+    tabsetPanel(type = "tabs", id = ns("reporting_tabset"),
+                tabPanel("Instructions", 
+                         id = ns("instructions"),
+                         value = "instructions",
+                         textOutput(ns("report_instructions"))),
+                tabPanel("Logs", 
+                         id = ns("logs"),
+                         value = "logs",
+                         actionButton(ns("logs_refresh"),
+                                      label = "",
+                                      icon = icon("refresh")) %>% 
+                           tagAppendAttributes(style = "float:right;"),
+                         dataTableOutput(ns("report_logs")))
     )
  
   )
@@ -23,7 +32,7 @@ mod_reporting_ui <- function(id){
 #'
 #' @noRd 
 mod_reporting_server <- function(id, project){
-  moduleServer( id, function(input, output, session){
+  moduleServer(id, function(input, output, session){
     ns <- session$ns
     
     # instructions ------------
@@ -31,14 +40,23 @@ mod_reporting_server <- function(id, project){
       "Instructions for using this module..."
     )
     
+    logs_df <- eventReactive(input$reporting_tabset == "logs" | input$logs_refresh, {
+     
+      if(isTruthy(project()$active_project)){
+        load_logs_for_reporting(project()$project_db,
+                                project()$active_project) %>% 
+          dplyr::arrange(dplyr::desc(created_at))
+      }else{""}
+    })
+  
     # logs ------------
     output$report_logs <- renderDataTable({
-      load_logs_for_reporting(project()$project_db,
-                              project()$active_project)
+      req(logs_df())
+      logs_df() %>% 
+        dplyr::mutate(payload = purrr::map_chr(.data$payload, parse_payload_json))
     })
- 
-  })
-}
+    
+})}
     
 ## To be copied in the UI
 # mod_reporting_ui("reporting_ui_1")
