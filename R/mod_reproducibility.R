2b9aa3c04550801d65598e336e7acce3b2bbc5da
Michael Škvrňák <397925@mail.muni.cz>
2022-05-10 15:29:51 +0200

    Add select input

--- R/mod_reproducibility.R
+++ R/mod_reproducibility.R
@@ -10,9 +10,12 @@
 mod_reproducibility_ui <- function(id){
   ns <- NS(id)
   tagList(
-
-      actionButton(ns("test"), "test"),
-      textOutput(ns("mean_no_codes"))
+    selectInput(ns("metrics_select"), 
+                "Select metrics", 
+                choices = c("", "Total overlap" = "total", "Overlap by code" = "by_code")),
+    actionButton(ns("test"), "Calculate"),
+    textOutput(ns("mean_no_codes")), 
+    tableOutput(ns("mean_by_codes"))
   )
 }
     
@@ -23,27 +26,42 @@ mod_reproducibility_server <- function(id, project){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     
-    output$mean_no_codes <- eventReactive(input$test, {
-      browser()
-      
-      segments <- load_all_segments_db(project_db = project()$project_db, 
-                                       active_project = project()$active_project)
-      # TODO: update function
-      segments %>% 
-        dplyr::count(user_id) %>% 
-        dplyr::pull(n) %>% 
-        mean()
-      
-      # 1. kolik % segmentů má překryv a kolik ne → zobrazit prostý referenční překryv, tzn. číslo 
-      # (zohlednit nějak počet kodérů) a jeden dokument se všemi kódovanými texty pod sebou; dvěma 
-      # barvami vyznačené překrývající a nepřekrývající segmenty
+    observe({
+      if(input$metrics_select == "total"){
+        output$mean_no_codes <- eventReactive(input$test, {
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          
+          # TODO: update function
+          # 1. kolik % segmentů má překryv a kolik ne → zobrazit prostý referenční překryv, tzn. číslo 
+          # (zohlednit nějak počet kodérů) a jeden dokument se všemi kódovanými texty pod sebou; dvěma 
+          # barvami vyznačené překrývající a nepřekrývající segmenty
+          
+          segments %>% 
+            dplyr::count(user_id) %>% 
+            dplyr::pull(n) %>% 
+            mean()
+        })
+      }
       
       # 2. stejný výpočet a vizualizaci pro každý kód
-      
-      # 3. pro vybraný kód spočítat, kolik % segmentů má překryv dvou, tří a čtyř kodérů + 
-      # heatmapa hodnot překryvu jednotlivých kodérů
-      
+      if(input$metrics_select == "by_code"){
+        observeEvent(input$test, {
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          
+          output$mean_by_codes <- renderTable({segments %>% 
+            dplyr::count(code_id, user_id) %>% 
+            dplyr::group_by(code_id) %>% 
+            dplyr::summarise(mean = mean(n), .groups = "drop")})
+        })
+        
+      }
     })
+
+    
+    # 3. pro vybraný kód spočítat, kolik % segmentů má překryv dvou, tří a čtyř kodérů + 
+    # heatmapa hodnot překryvu jednotlivých kodérů
     
     
     
