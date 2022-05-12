61d9569f0196068be7b8e4382f961d6186f00519
Michael Škvrňák <397925@mail.muni.cz>
2022-05-12 21:23:25 +0200

    Add overlap by segments

--- R/mod_reproducibility.R
+++ R/mod_reproducibility.R
@@ -13,10 +13,14 @@ mod_reproducibility_ui <- function(id){
     selectInput(ns("metrics_select"), 
                 "Select metrics", 
                 choices = c("", 
-                            "Total overlap" = "total", 
-                            "Overlap by code" = "by_code", 
-                            "Overlap by coder" = "by_user", 
-                            "Overlap by coder and code" = "by_user_code")),
+                            "Total overlap [character]" = "total", 
+                            "Overlap by code [character]" = "by_code", 
+                            "Overlap by coder [character]" = "by_user", 
+                            "Overlap by coder and code [character]" = "by_user_code", 
+                            "Total overlap [segments]"="total_segment", 
+                            "Overlap by code [segments]" = "by_code_segment", 
+                            "Overlap by coder [segments]" = "by_user_segment", 
+                            "Overlap by coder and code [segments]" = "by_user_code_segment")),
     actionButton(ns("test"), "Calculate"),
     uiOutput(ns("overlap_table")), 
     plotOutput(ns("overlap_plot"))
@@ -52,6 +56,22 @@ mod_reproducibility_server <- function(id, project){
       })
       }
       
+      if(input$metrics_select == "total_segment"){
+        observeEvent(input$test, {
+          
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          
+          overlap_df <- calculate_segment_overlap_by_users(segments) %>% 
+            dplyr::summarise(total_overlap = mean(is_overlap), 
+                             n_segments = length(unique(segment_id)), 
+                             n_coders = length(unique(c(coder1_id, coder2_id))))
+          
+          output$overlap_plot <- NULL
+          output$overlap_table <- renderTable({overlap_df})
+        })
+      }
+      
       # 2. stejný výpočet a vizualizaci pro každý kód
       if(input$metrics_select == "by_code"){
         observeEvent(input$test, {
@@ -71,6 +91,29 @@ mod_reproducibility_server <- function(id, project){
             dplyr::select(code_name, w_total_overlap, 
                           n_char_coded, n_coders) %>% 
             dplyr::arrange(dplyr::desc(w_total_overlap))
+          
+          output$overlap_plot <- NULL
+          output$overlap_table <- renderTable({overlap_df})
+        })
+      }
+      
+      if(input$metrics_select == "by_code_segment"){
+        observeEvent(input$test, {
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          
+          codes <- load_codes_names(active_project = project()$active_project, 
+                                    project_db = project()$project_db)
+          
+          overlap_df <- calculate_segment_overlap_by_users(segments) %>% 
+            dplyr::group_by(code_id) %>% 
+            dplyr::summarise(total_overlap = mean(is_overlap), 
+                             n_segments = length(unique(segment_id)), 
+                             n_coders = length(unique(c(coder1_id, coder2_id)))) %>% 
+            dplyr::left_join(., codes, by = "code_id") %>% 
+            dplyr::select(code_name, total_overlap, 
+                          n_segments, n_coders) %>% 
+            dplyr::arrange(dplyr::desc(total_overlap))
           
           output$overlap_plot <- NULL
           output$overlap_table <- renderTable({overlap_df})
@@ -124,6 +167,54 @@ mod_reproducibility_server <- function(id, project){
         })
       }
       
+      if(input$metrics_select == "by_user_segment"){
+        observeEvent(input$test, {
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          
+          users <- load_users_names(project_db = project()$project_db, 
+                                    active_project = project()$active_project)
+          
+          overlap_df <- calculate_segment_overlap_by_users(segments) %>% 
+            dplyr::left_join(., users %>% dplyr::rename(coder1_name = user_name), 
+                             by = c("coder1"="user_id")) %>% 
+            dplyr::left_join(., users %>% dplyr::rename(coder2_name = user_name), 
+                             by = c("coder2"="user_id")) %>% 
+            dplyr::group_by(coder1_name, coder2_name, coder1, coder2) %>% 
+            dplyr::summarise(total_overlap = mean(is_overlap), 
+                             .groups = "drop") %>% 
+            dplyr::rename(coder1_id = coder1, coder2_id = coder2)
+          
+          # make it symmetrical
+          overlap_df <- dplyr::bind_rows(overlap_df, 
+                                         overlap_df %>% 
+                                           dplyr::rename(coder2_id2 = coder1_id, 
+                                                         coder2_name2 = coder1_name, 
+                                                         coder1_id2 = coder2_id, 
+                                                         coder1_name2 = coder2_name) %>% 
+                                           dplyr::rename(coder2_id = coder2_id2, 
+                                                         coder1_id = coder1_id2, 
+                                                         coder1_name = coder1_name2, 
+                                                         coder2_name = coder2_name2)
+          ) %>% unique()
+          
+          overlap_heatmap <- ggplot2::ggplot(overlap_df, 
+                                             ggplot2::aes(x = factor(coder1_name), 
+                                                          y = factor(coder2_name), 
+                                                          fill = total_overlap)) + 
+            ggplot2::geom_tile() + 
+            ggplot2::scale_fill_viridis_c(limits = c(0, 1)) + 
+            ggplot2::theme_minimal() + 
+            ggplot2::labs(x = "Coder 1", 
+                          y = "Coder 2", 
+                          fill = "Overlap") + 
+            ggplot2::coord_fixed()
+          
+          output$overlap_table <- NULL
+          output$overlap_plot <- renderPlot({overlap_heatmap})
+        })
+      }
+      
       if(input$metrics_select == "by_user_code"){
         observeEvent(input$test, {
           segments <- load_all_segments_db(project_db = project()$project_db, 
@@ -139,6 +230,55 @@ mod_reproducibility_server <- function(id, project){
             dplyr::left_join(., users %>% dplyr::rename(coder2_name = user_name), 
                              by = c("coder2_id"="user_id")) %>% 
             dplyr::left_join(., codes, by = "code_id")
+          
+          overlap_df <- dplyr::bind_rows(overlap_df, 
+                                         overlap_df %>% 
+                                           dplyr::rename(coder2_id2 = coder1_id, 
+                                                         coder2_name2 = coder1_name, 
+                                                         coder1_id2 = coder2_id, 
+                                                         coder1_name2 = coder2_name) %>% 
+                                           dplyr::rename(coder2_id = coder2_id2, 
+                                                         coder1_id = coder1_id2, 
+                                                         coder1_name = coder1_name2, 
+                                                         coder2_name = coder2_name2)
+          ) %>% unique()
+          
+          overlap_heatmap <- ggplot2::ggplot(overlap_df, 
+                                             ggplot2::aes(x = factor(coder1_name), 
+                                                          y = factor(coder2_name), 
+                                                          fill = total_overlap)) + 
+            ggplot2::geom_tile() + 
+            ggplot2::scale_fill_viridis_c(limits = c(0, 1)) + 
+            ggplot2::theme_minimal() + 
+            ggplot2::labs(x = "Coder 1", 
+                          y = "Coder 2", 
+                          fill = "Overlap") + 
+            ggplot2::coord_fixed() + 
+            ggplot2::facet_wrap(ggplot2::vars(code_name))
+          
+          output$overlap_table <- NULL
+          output$overlap_plot <- renderPlot({overlap_heatmap})
+        })
+      }
+      
+      if(input$metrics_select == "by_user_code_segment"){
+        observeEvent(input$test, {
+          segments <- load_all_segments_db(project_db = project()$project_db, 
+                                           active_project = project()$active_project)
+          users <- load_users_names(project_db = project()$project_db, 
+                                    active_project = project()$active_project)
+          codes <- load_codes_names(project_db = project()$project_db, 
+                                    active_project = project()$active_project)
+          
+          overlap_df <- calculate_segment_overlap_by_users(segments) %>% 
+            dplyr::group_by(code_id, coder1, coder2) %>% 
+            dplyr::summarise(total_overlap = mean(is_overlap)) %>% 
+            dplyr::left_join(., users %>% dplyr::rename(coder1_name = user_name), 
+                             by = c("coder1"="user_id")) %>% 
+            dplyr::left_join(., users %>% dplyr::rename(coder2_name = user_name), 
+                             by = c("coder2"="user_id")) %>% 
+            dplyr::left_join(., codes, by = "code_id") %>% 
+            dplyr::rename(coder1_id = coder1, coder2_id = coder2)
           
           overlap_df <- dplyr::bind_rows(overlap_df, 
                                          overlap_df %>% 
--- R/mod_reproducibility_utils_reproducibility.R
+++ R/mod_reproducibility_utils_reproducibility.R
@@ -1,5 +1,5 @@
 load_all_segments_db <- function(active_project, project_db) {
-    code_id <- segment_start <- segment_end <- NULL
+    code_id <- segment_start <- segment_end <- segment_id <- NULL
     if (isTruthy(active_project)) {
         
         con <- DBI::dbConnect(RSQLite::SQLite(),
@@ -11,6 +11,7 @@ load_all_segments_db <- function(active_project, project_db) {
             dplyr::select(user_id, 
                           code_id,
                           doc_id, 
+                          segment_id, 
                           segment_start,
                           segment_end) %>%
             dplyr::collect()
@@ -97,5 +98,63 @@ calculate_code_overlap_by_users <- function(segments){
                              n_char = dplyr::n()) %>% 
             dplyr::mutate(total_overlap = 1 - ((coder1_missing_char + coder2_missing_char) / n_char))
     })
+}
+
+calculate_segment_overlap_by_users <- function(segments){
+    unique_coders <- segments %>% 
+        dplyr::pull(user_id) %>% unique()
     
+    # Create a combination of all coders who coded something
+    coders_grid <- as.data.frame(t(combn(unique_coders, m = 2))) %>% 
+        dplyr::rename(coder1_id = V1, coder2_id = V2)
+    
+    purrr::map_df(seq_len(nrow(coders_grid)), function(x) {
+        coder1_id <- coders_grid$coder1_id[x]
+        coder2_id <- coders_grid$coder2_id[x]
+        
+        coded_segments_by_two <- segments %>% 
+            dplyr::filter(user_id %in% c(coder1_id, coder2_id))
+        
+        coded_segments_by_two <- segments %>% 
+            dplyr::filter(user_id %in% c(coder1_id, coder2_id))
+        
+        fst_coder <- coded_segments_by_two %>% 
+            dplyr::filter(user_id == coder1_id) %>% 
+            dplyr::mutate(segment_vector = purrr::map2(segment_start, segment_end, ~c(.x:.y))) %>% 
+            tidyr::unnest(., segment_vector)
+        
+        snd_coder <- coded_segments_by_two %>% 
+            dplyr::filter(user_id == coder2_id) %>% 
+            dplyr::mutate(segment_vector = purrr::map2(segment_start, segment_end, ~c(.x:.y))) %>% 
+            tidyr::unnest(., segment_vector)
+        
+        tmp <- dplyr::full_join(
+            fst_coder %>% dplyr::select(coder1 = user_id, coder1_segment_id = segment_id, 
+                                        doc_id, code_id, segment_vector), 
+            snd_coder %>% dplyr::select(coder2 = user_id, coder2_segment_id = segment_id, 
+                                        doc_id, code_id, segment_vector), 
+            by = c("segment_vector", "doc_id", "code_id")) 
+        
+        coder1 <- tmp %>% 
+            dplyr::ungroup() %>% 
+            dplyr::group_by(doc_id, code_id, coder1, coder1_segment_id) %>% 
+            dplyr::summarise(overlap = length(segment_vector), 
+                             is_overlap = any(!is.na(coder2) & !is.na(coder1_segment_id)), 
+                             .groups = "drop") %>% 
+            dplyr::rename(segment_id = coder1_segment_id) %>% 
+            dplyr::mutate(coder1 = coder1_id, 
+                          coder2 = coder2_id)
+        
+        coder2 <- tmp %>% 
+            dplyr::ungroup() %>% 
+            dplyr::group_by(doc_id, code_id, coder2, coder2_segment_id) %>% 
+            dplyr::summarise(overlap = length(segment_vector), 
+                             is_overlap = any(!is.na(coder1) & !is.na(coder2_segment_id)), 
+                             .groups = "drop") %>%
+            dplyr::rename(segment_id = coder2_segment_id) %>% 
+            dplyr::mutate(coder1 = coder1_id, 
+                          coder2 = coder2_id)
+        
+        dplyr::bind_rows(coder1, coder2)
+    })
 }
