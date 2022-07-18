c656d4a68304b52578302d65ab09b4c59859e28e
Michael Škvrňák <michael.skvrnak@protonmail.com>
2022-07-18 23:03:35 +0200

    Fix tests

--- tests/testthat/shinytest/tests/shinytest/select-project.R
+++
@@ -1,6 +0,0 @@
-app <- ShinyDriver$new("../../")
-app$snapshotInit("select-project")
-
-app$setInputs(`launchpad_loader_ui_1-project_selector_load` = "1")
-app$setInputs(`launchpad_loader_ui_1-project_load` = "click")
-app$snapshot()
