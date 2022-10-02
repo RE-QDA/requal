dda8329178b7b0cccc88b17ea2fd81f3416bbf62
hlageek <radim.hladik@fulbrightmail.org>
2022-10-02 15:55:51 +0200

    renv update

--- R/app_ui.R
+++ R/app_ui.R
@@ -7,35 +7,6 @@
 #' @noRd
 
 app_ui_setup <- function(request) {
-<<<<<<< Updated upstream
-  tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic
-    dashboardPage(
-      title = "ReQual",
-      options = list(sidebarExpandOnHover = FALSE),
-      header = dashboardHeader(
-        title = tags$span(
-          tags$img(
-            src = "www/requal_logo.png",
-            height = "70%", style = "margin-right: 20px"
-          ), "ReQual"
-        ),
-        tags$li(mod_user_ui("user_ui_1")) %>%
-          tagAppendAttributes(class = "dropdown"),
-        controlbarIcon = icon("ellipsis-v", id = "launchpad_icon"),
-        dropdownBlock2(
-          id = "btn-memo",
-          badgeStatus = NULL,
-          icon = shiny::icon("sticky-note-o", verify_fa = FALSE),
-          mod_memo_ui("memo_ui_1")
-        ) %>% tagAppendAttributes(class = "memo")
-      ),
-      sidebar = set_left_menu(),
-      body = set_dashboard_body(),
-      controlbar = set_controlbar()
-=======
     
     tagList(
         # Leave this function for adding external resources
@@ -60,29 +31,12 @@ app_ui_setup <- function(request) {
                       body = set_dashboard_body(),
                       controlbar = set_controlbar()
         )
->>>>>>> Stashed changes
     )
-  )
 }
 
 if (get_golem_config("mode") == "local") {
   app_ui <- app_ui_setup
 } else {
-<<<<<<< Updated upstream
-  # define some credentials
-  credentials <- data.frame(
-    user = c("admin", "requal"), # mandatory
-    password = c("admin", "requal"), # mandatory
-    start = c("2019-04-15"), # optional (all others)
-    expire = c(NA, "2022-12-31"),
-    admin = c(FALSE, TRUE),
-    comment = "Simple and secure authentification mechanism
-  for single 'Shiny' applications.",
-    stringsAsFactors = FALSE
-  )
-
-  app_ui <- shinymanager::secure_app(app_ui_setup)
-=======
     print(get_golem_config("mode"))
     print(get_golem_config("access"))
     if (!file.exists(get_golem_config("access"))) {
@@ -93,11 +47,12 @@ if (get_golem_config("mode") == "local") {
         password = c("admin", "user"),
         # password will automatically be hashed
         admin = c(TRUE, FALSE),
+        user_id = c(1,2),
         stringsAsFactors = FALSE
     )
     
-    # you can use keyring package to set database key
-    keyring::key_set("requal-access-key", "requal")
+    # use keyring package to set database key
+    keyring::key_set("requal-access-key", "requal", prompt = "Set password for users' database:")
     
     # Init the database
     shinymanager::create_db(
@@ -113,7 +68,6 @@ if (get_golem_config("mode") == "local") {
                                        enable_admin = TRUE, 
                                        fab_position = "bottom-left"
                                        )
->>>>>>> Stashed changes
 }
 
 #' Add external Resources to the Application
