b0a1bc11bab0465d1dd3600e7cd722e0d9ffbc9c
hlageek <22201406+hlageek@users.noreply.github.com>
2023-02-10 07:29:27 +0900

    basic ui

--- R/mod_use_manager_utils_use_manager.R
+++ R/mod_use_manager_utils_use_manager.R
@@ -1,3 +1,4 @@
+# read users from credentials DB
 get_users <- function() {
   credentials_con <- DBI::dbConnect(
     RSQLite::SQLite(),
@@ -9,4 +10,30 @@ get_users <- function() {
   shinymanager::read_db_decrypt(credentials_con,
     passphrase = golem::get_golem_options(which = "credentials_pass")
   )[c("user", "user_id")]
+}
+
+
+# Generate user permissions UI -----
+gen_permissions_ui <- function(user_id,
+                         permission,
+                         initial_value) {
+    # permission_name <- switch(permission,
+    # can_code = "Kódovat",
+    # can_modify_codes = "ddd",
+    # can_delete_codes = "df",
+    # can_modify_documents = "sl",
+    # can_delete_documents = "s",
+    # can_manage = "d"
+    # )
+   
+    checkboxGroupInput(
+        inputId = as.character(user_id),
+    label = as.character(user_id),
+    choiceNames = unname(permission),
+    choiceValues = unname(initial_value),
+    inline = TRUE
+    ) %>% tagAppendAttributes(
+        `data-user_id` = user_id
+    ) 
+
 }
