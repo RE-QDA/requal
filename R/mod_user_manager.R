7d31a0bac62dfeea44f00c24040e3091f1c1cfc0
hlageek <22201406+hlageek@users.noreply.github.com>
2023-03-13 10:24:05 +0100

    conditional save permissions btn

--- R/mod_user_manager.R
+++ R/mod_user_manager.R
@@ -10,10 +10,27 @@
 mod_user_manager_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    uiOutput(ns("user_management_buttons_ui")),
+    if (golem::get_golem_options("mode") == "server") {
+      div(
+        menu_btn(
+          uiOutput(ns("add_user_ui")),
+          label = "Add user",
+          icon = "plus"
+        ),
+        menu_btn(
+          uiOutput(ns("remove_user_ui")),
+          label = "Remove user",
+          icon = "minus"
+        )
+      ) %>% tagAppendAttributes(style = "display: -webkit-inline-box;")
+    },
     tags$h2("Project members"),
     uiOutput(ns("assigned_users")),
-    uiOutput(ns("user_save_permissions_ui"))
+    if (golem::get_golem_options("mode") == "server") {
+      conditionalPanel("output.permissions_modify == 1",
+      actionButton(ns("save_permissions"), "Save permissions"), ns = ns
+      )
+    }
   )
 }
 
@@ -24,135 +41,126 @@ mod_user_manager_server <- function(id, glob) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     loc <- reactiveValues()
-    
+
     # initialize user management page on project load ----
     observeEvent(glob$active_project, {
-      
-      if(golem::get_golem_options("mode") == "server"){
+      if (golem::get_golem_options("mode") == "server") {  
         # on init, get a list of users in credentials
         loc$all_users <- get_users(
           credentials_path = golem::get_golem_options(which = "credentials_path"),
           credentials_pass = golem::get_golem_options(which = "credentials_pass")
-        ) 
-        
+        )
+
         loc$all_users_choices <- stats::setNames(loc$all_users$user_id, loc$all_users$user_login)
-        
+
         loc$users_permissions_df <- get_user_permissions(
-          glob$pool, 
+          glob$pool,
           glob$active_project
         )
         
-        loc$project_members_ids <- loc$users_permissions_df %>% 
-          dplyr::filter(!permissions_modify) %>% 
-          dplyr::mutate(user_id = stats::setNames(user_id, user_name)) %>% 
+        loc$project_members_ids <- loc$users_permissions_df %>%
+          dplyr::mutate(user_id = stats::setNames(user_id, user_name)) %>%
           dplyr::pull(user_id)
-        
       }
-      
+
       # add user permission to glob
-      # glob$user$permissions <- loc$users_permissions_df %>% 
-      #   dplyr::filter(user_id == glob$user$user_id) %>% 
+      # glob$user$permissions <- loc$users_permissions_df %>%
+      #   dplyr::filter(user_id == glob$user$user_id) %>%
       #   dplyr::select(dplyr::any_of(
-      #     c("data_modify", "data_other_modify", "data_other_view", 
-      #       "attributes_modify", "attributes_other_modify", "attributes_other_view", 
-      #       "codebook_modify", "codebook_other_modify", 
-      #       "annotation_modify", "annotation_other_modify", "annotation_other_view", 
-      #       "analysis_other_view", "report_other_view", "permissions_modify"))) %>% 
+      #     c("data_modify", "data_other_modify", "data_other_view",
+      #       "attributes_modify", "attributes_other_modify", "attributes_other_view",
+      #       "codebook_modify", "codebook_other_modify",
+      #       "annotation_modify", "annotation_other_modify", "annotation_other_view",
+      #       "analysis_other_view", "report_other_view", "permissions_modify"))) %>%
       #   as.list()
     })
-    
-    output$user_management_buttons_ui <- renderUI({
-      if(golem::get_golem_options("mode") == "server" && 
-        req(glob$user$data$permissions_modify) == 1){
-        div(
-          menu_btn(
-            uiOutput(ns("add_user_ui")),
-            label = "Add user",
-            icon = "plus"
-          ),
-          menu_btn(
-            uiOutput(ns("remove_user_ui")),
-            label = "Remove user",
-            icon = "minus"
-          )
-        ) %>% tagAppendAttributes(style = "display: -webkit-inline-box;") 
-      }
-    })
-    outputOptions(output, "user_management_buttons_ui", suspendWhenHidden = FALSE)
 
-    output$user_save_permissions_ui <- renderUI({
-      if(golem::get_golem_options("mode") == "server" && 
-         glob$user$data$permissions_modify == 1){
-        actionButton(ns("save_permissions"), "Save permissions")
-      }
-    })
-    
-    # render project members ----
+    # hide save permission UI from users without sufficient permission ======
+      output$permissions_modify <- eventReactive(loc$users_permissions_df, { 
+        loc$permissions_modify <- loc$users_permissions_df %>%
+          dplyr::filter(user_id == glob$user$user_id) %>%
+          dplyr::pull(permissions_modify)
+        })
+      outputOptions(output, "permissions_modify", suspendWhenHidden = FALSE)
+
+      
+
+    # render project members =======================================================
     output$assigned_users <- renderUI({
-      if(golem::get_golem_options("mode") == "server"){
-        
+      if (golem::get_golem_options("mode") == "server") {
         loc$users_permissions_long <- loc$users_permissions_df %>%
-          # dplyr::select(-permissions_modify) %>% 
+          # dplyr::select(-permissions_modify) %>%
           dplyr::select(user_id, tidyselect::matches("view|modify")) %>%
           tidyr::pivot_longer(
             -user_id,
             names_to = "permission",
             values_to = "value"
-          ) 
+          )
         # create nested df for nested UI
-        users_permissions_nested <- loc$users_permissions_long %>% 
+        users_permissions_nested <- loc$users_permissions_long %>%
           dplyr::mutate(user_id_copy = user_id) %>%
           tidyr::nest(data = -user_id_copy) %>%
           dplyr::inner_join(loc$users_permissions_df,
-                            by = c("user_id_copy" = "user_id")
-          ) %>% 
+            by = c("user_id_copy" = "user_id")
+          ) %>%
           dplyr::filter(!duplicated(user_id_copy))
-        
+
         # generated user boxes with nested permissions
         gen_users_permissions_ui(users_permissions_nested, id = id, glob$user$data)
+      } else {
+        "Local version of reQual does not support multiple users."
       }
     })
-    
-    # change permissions ----
+
+    # change permissions =======================================================
     observeEvent(input$save_permissions, {
+      check_modify_permission(
+        loc$permissions_modify,
+        "Missing permission to modify permissions."
+      )
       loc$users_permissions_long <- loc$users_permissions_long %>%
         dplyr::mutate(value = purrr::map2_int(user_id, permission,
-                                              .f = function(user_id, permission) {
-                                                input[[paste(user_id, permission, sep = "_")]]
-                                              }
+          .f = function(user_id, permission) {
+            input[[paste(user_id, permission, sep = "_")]]
+          }
         ))
-      
+
       modify_permissions_record(
         pool = glob$pool,
         project_id = glob$active_project,
         permissions_df = loc$users_permissions_long
       )
-     loc$users_permissions_df <- get_user_permissions(
-        glob$pool, 
+      loc$users_permissions_df <- get_user_permissions(
+        glob$pool,
         glob$active_project
       )
       showNotification("Changes to permissions were saved.")
     })
-    
-    # add new users ----
+
+    # add new users =======================================================
     observeEvent(input$assign, {
-     
+      check_modify_permission(
+        loc$permissions_modify,
+        "Missing permission to add users."
+      )
+
       req(input$rql_users > 0)
       # run check on existing users
       existing_users <- dplyr::tbl(glob$pool, "users") %>%
         dplyr::pull(user_id)
-      
+
       existing_users_check <- input$rql_users[!input$rql_users %in% existing_users]
-      
+
       if (length(existing_users_check) > 0) {
         # create user in db if an uknown user is assigned
-        users_df <- loc$all_users %>% 
+        users_df <- loc$all_users %>%
           dplyr::filter(user_id %in% as.integer(input$rql_users))
         purrr::map(users_df$user_id, .f = function(x) {
-          users_df_filtered <- users_df %>% 
+          users_df_filtered <- users_df %>%
             dplyr::filter(user_id == as.integer(x))
           DBI::dbWriteTable(glob$pool, "users", users_df_filtered,
-                            append = TRUE, row.names = FALSE)
+            append = TRUE, row.names = FALSE
+          )
         })
       }
       add_permissions_record(
@@ -162,18 +170,22 @@ mod_user_manager_server <- function(id, glob) {
       )
       # refresh users for current project
       loc$users_permissions_df <- get_user_permissions(
-        glob$pool, 
+        glob$pool,
         glob$active_project
       )
     })
-    
-    # remove users ----
+
+    # remove users =======================================================
     observeEvent(input$remove_members, {
+      check_modify_permission(
+        loc$permissions_modify,
+        "Missing permission to remove users."
+      )
       owner_check <- loc$users_permissions_df %>%
         dplyr::filter(
           project_id %in% glob$active_project &
-          user_id %in% req(input$members_to_remove)
-          ) %>%
+            user_id %in% req(input$members_to_remove)
+        ) %>%
         dplyr::pull(project_owner)
       if (any(owner_check == 1)) warn_user("Project owners cannot be removed from project.")
       req(all(owner_check == 0))
@@ -182,28 +194,29 @@ mod_user_manager_server <- function(id, glob) {
         project_id = glob$active_project,
         user_id = req(input$members_to_remove)
       )
-      
+
       # refresh users for current project
       loc$project_members_ids <- loc$project_members_ids[loc$project_members_ids != as.integer(input$members_to_remove)]
-      
+
       loc$users_permissions_df <- get_user_permissions(
-        glob$pool, 
+        glob$pool,
         glob$active_project
       )
     })
-    
-    # update user selection inputs ----
+
+    # update user selection inputs =======================================================
     observeEvent(c(loc$users_permissions_df, glob$active_project), {
-      
       loc$users_to_add <- loc$all_users_choices[!loc$all_users_choices %in% as.integer(loc$users_permissions_df$user_id)]
-      if (length(loc$users_to_add) < 1) {loc$users_to_add <- c("All registered users have been assigned." = 0)}
+      if (length(loc$users_to_add) < 1) {
+        loc$users_to_add <- c("All registered users have been assigned." = 0)
+      }
       # display users to add
       updateSelectInput(
         session = session,
         "rql_users",
         choices = c("", loc$users_to_add)
       )
-      
+
       # display users to remove
       updateSelectInput(
         session = session,
@@ -211,18 +224,17 @@ mod_user_manager_server <- function(id, glob) {
         choices = c("", stats::setNames(
           loc$users_permissions_df$user_id,
           loc$users_permissions_df$user_name
-        )
-        )
+        ))
       )
     })
-    
-    # Add user UI --------------
+
+    # Add user UI =======================================================
     output$add_user_ui <- renderUI({
       add_user_UI(id)
     })
     outputOptions(output, "add_user_ui", suspendWhenHidden = FALSE)
-    
-    # Remove user UI --------------
+
+    # Remove user UI =======================================================
     output$remove_user_ui <- renderUI({
       remove_user_UI(id)
     })
