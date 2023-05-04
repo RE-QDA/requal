07f3a4b29a3182cdae7e0c2f34b1c36bf2b889c4
Michael Škvrňák <michael.skvrnak@protonmail.com>
2023-05-04 14:10:31 +0200

    Fix superfluous parameter

--- R/mod_user_attributes_utils.R
+++ R/mod_user_attributes_utils.R
@@ -1,6 +1,6 @@
 utils::globalVariables(c("attribute_id", "attribute_name", "attribute_value_id", "attribute_value")) 
 
-add_attribute <- function(pool, attribute_name, type = "categorical", object, project_id, user_id){
+add_attribute <- function(pool, attribute_name, type = "categorical", object, project_id){
     new_attribute <- data.frame(
         attribute_name = attribute_name, 
         attribute_object = object, 
