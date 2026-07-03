9844ed5caf563cde232f7faec8fd9c45b4276ee9
hlageek <22201406+hlageek@users.noreply.github.com>
2026-07-03 22:31:21 +0200

    prevent NULL user_login

--- R/db_startup.R
+++ R/db_startup.R
@@ -404,9 +404,15 @@ create_default_user <- function(pool, project_id, user_id) {
   )
 
   if (golem::get_golem_options("mode") %in% c("local", "local_test")) {
+    user_name <- unname(Sys.info()["user"])
+    user_login <- unname(Sys.info()["user"])
+    if (is.null(user_login)) {
+      user_login <- "requal_user"
+    }
+
     user_df <- tibble::tibble(
-      user_name = unname(Sys.info()["user"]),
-      user_login = unname(Sys.info()["user"])
+      user_name = user_name,
+      user_login = user_login
     )
 
     DBI::dbWriteTable(pool, "users", user_df, append = TRUE, row.names = FALSE)
